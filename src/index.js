fc30c915f1ab2c1011e9e8c0e00f6c1b7a05c032
Sushant Kumar Srivastava <sushant.kumar.srivastava@aibel.com>
2025-06-28 11:34:54 +0200

    mer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
 
 import "./index.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./assets/font-awesome/css/all.css";
@@ -13,15 +13,16 @@ import "./assets/font-awesome/css/all.css";
 const engine = new Styletron();
 
 ReactDOM.render(
-    <StyletronProvider value={engine}>
-        <BaseProvider theme={LightTheme}>
-            <App />
-        </BaseProvider>
-    </StyletronProvider>,
+    <HashRouter>
+        <StyletronProvider value={engine}>
+            <BaseProvider theme={LightTheme}>
+                <App />
+            </BaseProvider>
+        </StyletronProvider>
+    </HashRouter>,              // second argument starts here
     document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want your app to work offline and load faster,
+// change unregister() to register() below.
 serviceWorker.unregister();
