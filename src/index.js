de3c7a1ba5c4671d89430e5982fe73f5eed1023b
Sushant Kumar Srivastava <sushant.kumar.srivastava@aibel.com>
2025-06-28 12:11:57 +0200

    mer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";      // ← change
+import { BrowserRouter } from "react-router-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
@@ -13,14 +13,16 @@ import "./assets/font-awesome/css/all.css";
 const engine = new Styletron();
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>     {/* ← change */}
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <StyletronProvider value={engine}>
             <BaseProvider theme={LightTheme}>
                 <App />
             </BaseProvider>
         </StyletronProvider>
-    </BrowserRouter>, {/* ← change */ }
-  document.getElementById("root")
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
+// If you want the app to work offline and load faster,
+// change unregister() to register() below.
 serviceWorker.unregister();
